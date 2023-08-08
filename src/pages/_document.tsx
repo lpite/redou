096468b0f7c69702373a28c841ebafc99346d464
lpite <farionsasa@gmail.com>
2023-08-08 12:00:58 +0300

    dark theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="uk" className="dark text-foreground bg-background">
       <Head />
-      <body>
+      <body className="container mx-auto">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
